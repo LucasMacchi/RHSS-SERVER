427b822654fea8c26997faefbc93e50001d86688
LucasMacchi <lucasmacchi25@gmail.com>
2025-07-08 10:20:24 -0300

    GET ALL LEGAJOS FIX

--- src/main/java/rhss_server/rhss_server/Interfaces/ILegajoRepo.java
+++ src/main/java/rhss_server/rhss_server/Interfaces/ILegajoRepo.java
@@ -3,6 +3,7 @@ package rhss_server.rhss_server.Interfaces;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import rhss_server.rhss_server.Tables.LegajosTable;
 
@@ -10,4 +11,8 @@ public interface ILegajoRepo extends JpaRepository<LegajosTable, Short>{
 
     List<LegajosTable> findByFullnameContaining(String nombre);
 
-    List<LegajosTable> findByEmpresa(String empresa);}
+    List<LegajosTable> findByEmpresa(String empresa);
+
+    @Query(value = "select * from glpi_rhss_legajos where fecha_egreso > NOW();", nativeQuery = true)
+    List<LegajosTable> findAllActivated();
+}
